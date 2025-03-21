8edea47e8c7a5519d27e72cbd753ea2497ff47b2
Jennifer Trinanes <jtrinanes@fullscale.ph>
2025-03-21 17:26:55 +0800

    Initial commit

---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventsList/GetEventsListQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery,
+        List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync())
+                .OrderBy(x => x.Date);
+
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
